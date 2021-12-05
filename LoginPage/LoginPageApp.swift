bc966cc3a42d72922c2d5ccb82da9dd4302e6a17
Yaroslav Yakovlev <jaroslawjakowlew@gmail.com>
2021-12-05 22:04:22 +0100

    Initial Commit

---
+++ LoginPage/LoginPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginPageApp.swift
+//  LoginPage
+//
+//  Created by Yaroslav on 5.12.21.
+//
+
+import SwiftUI
+
+@main
+struct LoginPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
